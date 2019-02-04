806dfe8f853d447f92e6c698210a96700cbe36fd
Andel Husbands <andelhusbands@Andels-MacBook-Pro.local>
2019-02-04 18:57:22 -0400

    add titles

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,10 @@ const IndexPage = () => (
   <Layout>
     <Heading />
     <PortfolioItem title="Mobile App & Website" copy="WhereDPump is a technology company that facilitates users through a mobile app, allowing them to see all upcoming events and take actions such as purchasing tickets and getting listed for clubs." color="wdp" url="https://www.wheredpump.com/img/assets/logos/logo_color.png" />
-    <PortfolioItem color="yup" url="https://yuplifett.com/static/media/logo.67146cf6.png" />
-    <PortfolioItem color="cmj" url="https://pensive-agnesi-079a44.netlify.com/static/logo-6f0ba42129ed8246738dcd9f47ab46cd.png" />
-    <PortfolioItem color="mei" url="https://andelhusbands.herokuapp.com/static/media/mei-logo.70c5d366.png" />
-    <PortfolioItem color="foodie" url="https://andelhusbands.herokuapp.com/static/media/logocolor.98f2be24.png" />
+    <PortfolioItem title="Website" color="yup" url="https://yuplifett.com/static/media/logo.67146cf6.png" />
+    <PortfolioItem title="Company Wesbite & Registration" color="cmj" url="https://pensive-agnesi-079a44.netlify.com/static/logo-6f0ba42129ed8246738dcd9f47ab46cd.png" />
+    <PortfolioItem title="Online Store" color="mei" url="https://andelhusbands.herokuapp.com/static/media/mei-logo.70c5d366.png" />
+    <PortfolioItem title="Web App" color="foodie" url="https://andelhusbands.herokuapp.com/static/media/logocolor.98f2be24.png" />
   </Layout>
 )
 
