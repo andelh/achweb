38d808c6c87435e88ebe396535d12170e0082de9
Andel Husbands <andelhusbands@gmail.com>
2022-01-24 18:03:22 -0400

    website can build!

--- components/branding-block.js
+++ components/branding-block.js
@@ -1,51 +1,52 @@
-import React, { Component } from 'react';
-import styled from 'styled-components'
-import Fade from 'react-reveal/Fade'
-import Zoom from 'react-reveal/Zoom'
+import React, { Component } from "react"
+import styled from "styled-components"
+import Fade from "react-reveal/Fade"
+import Zoom from "react-reveal/Zoom"
 
-import SectionTitle from './section-title';
+import SectionTitle from "./section-title"
 
-import djWorld from '../images/DJWorld.png'
-import ClaimButton from './claim-button';
-
-
+import ClaimButton from "./claim-button"
 
 const Block = styled.div`
-    padding: 50px 0;
-    max-width: 800px;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
+  padding: 50px 0;
+  max-width: 800px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
 `
 
 const Copy = styled.p`
-    text-align: center;
-    line-height: 150%;
+  text-align: center;
+  line-height: 150%;
 `
 
 const Image = styled.img`
-    margin: 0 auto;
-    width: 180px;
-    text-align: center;
-    display: block;
+  margin: 0 auto;
+  width: 180px;
+  text-align: center;
+  display: block;
 `
 
 class BrandingBlock extends Component {
-    state = {}
-    render() {
-        return (
-            <Fade delay={500}>
-                <Block>
-                    <SectionTitle>Powerful Branding</SectionTitle>
-                    <Copy>You already know the importance of a <b>STRONG</b> brand for your craft. We can help you elevate your brand and make it a global one by bringing it to the web.</Copy>
-                    <Zoom delay={300}>
-                        <Image src={djWorld} />
-                    </Zoom>
-                    <ClaimButton message="I'm ready to build my brand" />
-                </Block>
-            </Fade>
-        );
-    }
+  state = {}
+  render() {
+    return (
+      <Fade delay={500}>
+        <Block>
+          <SectionTitle>Powerful Branding</SectionTitle>
+          <Copy>
+            You already know the importance of a <b>STRONG</b> brand for your
+            craft. We can help you elevate your brand and make it a global one
+            by bringing it to the web.
+          </Copy>
+          <Zoom delay={300}>
+            <Image src="/DJWorld.png" />
+          </Zoom>
+          <ClaimButton message="I'm ready to build my brand" />
+        </Block>
+      </Fade>
+    )
+  }
 }
 
-export default BrandingBlock;
+export default BrandingBlock
