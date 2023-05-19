0fabc8b92c5ce21d7b126c34e06c8e23b0c67b85
Andel Husbands <andelhusbands@gmail.com>
2023-05-19 00:29:41 -0400

    update og-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata = {
   title: "Andel Husbands | Web and App Developer",
   description:
     "I'm a passionate web and software developer based in vibrant Trinidad and Tobago, and I'm here to turn your ideas into reality. With a strong desire to elevate the quality of products born from our home soil, I bring creativity, expertise, and a personal touch to every project. Let's connect and embark on a journey to create something truly remarkable.",
+  openGraph: {
+    images: "/og-image.png",
+  },
 }
 
 export default function RootLayout({
