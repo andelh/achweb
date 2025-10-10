60c86a09dfd24d2099f195a4ac1e27363e1b966a
Andel Husbands <andelhusbands@gmail.com>
2025-10-10 17:48:29 -0400

    Update FeaturedWork component to be asynchronous

--- components/home/featured-work.tsx
+++ components/home/featured-work.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import FeaturedWorkList from "../featured-work-list"
 
-export default function FeaturedWork() {
+export default async function FeaturedWork() {
   return (
     <div className="mb-10">
       <h2>See My Featured Work</h2>
